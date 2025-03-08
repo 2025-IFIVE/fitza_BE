9e401fe1b35cfb9ce16612724fc5758cd75e5b4c
ssorni <ssorni727@gmail.com>
2025-03-08 15:40:40 +0900

    begin : 프로젝트 셋팅

---
+++ fitza/src/main/java/com/ifive/fitza/FitzaApplication.java
@@ -0,0 +1,13 @@
+package com.ifive.fitza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitzaApplication.class, args);
+	}
+
+}
---
+++ fitza/src/test/java/com/ifive/fitza/FitzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifive.fitza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
